d43dd98069a8abf6672fdfe01491fbf4dcd304c0
kushpatel18 <120158336+kushpatel18@users.noreply.github.com>
2024-03-23 02:14:18 +0530

    Add files via upload

---
+++ js/_ssgManifest.js
@@ -0,0 +1,8 @@
+<html><head><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx/1.18.0 (Ubuntu)</center>
+<script defer="" src="https://static.cloudflareinsights.com/beacon.min.js/v84a3a4012de94ce1a686ba8c167c359c1696973893317" data-cf-beacon="{&quot;rayId&quot;:&quot;865b51cc5b3106be&quot;,&quot;b&quot;:1,&quot;version&quot;:&quot;2024.2.4&quot;,&quot;token&quot;:&quot;013acd90d43f4ea5bc5d2b25535abedc&quot;}" crossorigin="anonymous"></script>
+
+
+</body></html>
